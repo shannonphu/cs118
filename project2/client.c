9d39a0e5ab3fda9f07267a4efcbd89310042d5d0
shannonphu <shannonphu@sbcglobal.net>
2018-02-20 11:57:15 -0800

    client accepts filename arg and passes to server

--- project2/client.c
+++ project2/client.c
@@ -1,4 +1,3 @@
-
 /*
  A simple client in the internet domain using TCP
  Usage: ./client hostname port (./client 192.168.0.151 10000)
@@ -7,9 +6,11 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h>      // define structures like hostent
+#include <netdb.h>
 #include <stdlib.h>
 #include <strings.h>
+
+int MAX_PACKET_SIZE = 1024;
 
 void error(char *msg)
 {
@@ -19,21 +20,22 @@ void error(char *msg)
 
 int main(int argc, char *argv[])
 {
-    int sockfd; //Socket descriptor
+    int sockfd;
     int portno, n;
     struct sockaddr_in serv_addr;
-    struct hostent *server; //contains tons of information, including the server's IP address
+    struct hostent *server;
 
     char buffer[256];
-    if (argc < 3) {
-       fprintf(stderr,"usage %s hostname port\n", argv[0]);
+    if (argc < 4) {
+       fprintf(stderr,"usage ./%s hostname port filename\n", argv[0]);
        exit(0);
     }
     
     portno = atoi(argv[2]);
+    char *filename = argv[3];
 
     // SOCK_DGRAM is for an unreliable, connectionless UDP protocol
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0); //create a new socket
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     if (sockfd < 0) 
         error("ERROR opening socket");
     
@@ -48,25 +50,20 @@ int main(int argc, char *argv[])
     bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
     serv_addr.sin_port = htons(portno);
     
-    if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) //establish a connection to the server
+    if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0)
         error("ERROR connecting");
     
-    printf("Please enter the message: ");
-    bzero(buffer,256);
-    fgets(buffer,255,stdin);
-    
-    //n = send(sockfd,buffer,strlen(buffer),0); //send to the socket
-    n = write(sockfd,buffer,strlen(buffer)); //write to the socket
+    n = write(sockfd, filename, MAX_PACKET_SIZE);
     if (n < 0) 
          error("ERROR writing to socket");
     
     bzero(buffer,256);
-    n = read(sockfd,buffer,255); //read from the socket
+    n = read(sockfd, buffer, 255);
     if (n < 0) 
          error("ERROR reading from socket");
     printf("%s\n",buffer);
     
-    close(sockfd); //close socket
+    close(sockfd);
     
     return 0;
 }

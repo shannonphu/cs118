7d60550d37fb156631c1f4ff1a3b9413e406b2ec
shannonphu <shannonphu@sbcglobal.net>
2018-02-27 22:28:56 -0800

    packet serialization implemented

--- project2/util.h
+++ project2/util.h
@@ -19,6 +19,9 @@ struct Packet
 
 struct Packet* initPacket(const char *data) {
     struct Packet *newPacket = malloc(sizeof(struct Packet));
+    newPacket->sequenceNum = 2;
+    newPacket->ackNum = 1;
+    newPacket->flag = SYN;
     bzero(newPacket->payload, PAYLOAD_SIZE + 1);
     memcpy(newPacket->payload, data, PAYLOAD_SIZE);
     newPacket->payload[PAYLOAD_SIZE] = '\0';
@@ -27,4 +30,28 @@ struct Packet* initPacket(const char *data) {
 
 void destructPacket(struct Packet *packet) {
     free(packet);
+}
+
+// Encode packet to C string structured:
+//     sequenceNum | ackNum | flag | payload
+void packetToBytes(const struct Packet *packet, char *byteArray) {
+    memcpy(byteArray, &(packet->sequenceNum), sizeof(packet->sequenceNum));
+    memcpy(byteArray + sizeof(packet->sequenceNum), &(packet->ackNum), sizeof(packet->ackNum));
+    memcpy(byteArray + sizeof(packet->sequenceNum) + sizeof(packet->ackNum), &(packet->flag), sizeof(packet->flag));
+    memcpy(byteArray + sizeof(packet->sequenceNum) + sizeof(packet->ackNum) + sizeof(packet->flag), packet->payload, strlen(packet->payload));
+    
+    // fprintf(stderr, "seq %d\n", *byteArray);
+    // fprintf(stderr, "ack %d\n", *(byteArray + 4));
+    // fprintf(stderr, "flag %d\n", *(byteArray + 8));
+    // fprintf(stderr, "%s\n", byteArray + 12);
+}
+
+void bytesToPacket(struct Packet *packet, const char *byteArray) {
+    int sequenceNum = *byteArray;
+    packet->sequenceNum = sequenceNum;
+    int ackNum = *(byteArray + sizeof(packet->sequenceNum));
+    packet->ackNum = ackNum;
+    enum Flag flag = *(byteArray + sizeof(packet->sequenceNum) + sizeof(packet->ackNum));
+    packet->flag = flag;
+    strcpy(packet->payload, byteArray + sizeof(packet->sequenceNum) + sizeof(packet->ackNum) + sizeof(packet->flag));
 }
